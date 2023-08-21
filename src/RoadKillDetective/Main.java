9793f6d662dbd1da09eede7258c8f7a08ad5d299
Pablo <pablofguillen1503@gmail.com>
2023-08-21 12:41:44 +0200

    Solución ROAD KILL DETECTIVE

--- src/RoadKillDetective/Main.java
+++ src/RoadKillDetective/Main.java
@@ -32,30 +32,69 @@ public class Main {
     }
 
     private static String animalInPhoto(String photo){
-        boolean findAnimal = false;
-        int index = 0;
-        while(index < ANIMALS.size() && findAnimal == false){
-            String regExr = "^" + createRegExt(index) + "$";
-            System.out.println(regExr);
-
-            Pattern pattern = Pattern.compile(regExr);
-            if(isMatches(photo, pattern)){
-                findAnimal = true;
-                photo = ANIMALS.get(index);
-            }
-
-            index++;
-        }
-
-
-
-        if(findAnimal == false){
-            photo = "??";
-        }
+        photo = searchAnimalInPhoto(photo);
         return photo;
     }
 
-    private static boolean isMatches(String photo, Pattern pattern) {
+    private static String searchAnimalInPhoto(String photo){
+        int index = iterateOverAnimalList(photo);
+
+
+        if(index != -1){
+            photo = ANIMALS.get(index);
+        }
+
+        else{
+            photo = "??";
+        }
+
+
+        return photo;
+    }
+
+    private static int iterateOverAnimalList(String photo){
+        int index = 0;
+        boolean animalHasBeenRecognised = false;
+        while(animalHasntBeenRecognised(index, animalHasBeenRecognised)){
+            animalHasBeenRecognised = isAnimalInAnimalList(index, photo);
+            index++;
+        }
+
+        if(animalHasBeenRecognised){
+            return index;
+        }
+
+        else{
+            index = -1;
+            return index;
+        }
+    }
+    private static boolean animalHasntBeenRecognised(int index, boolean findAnimal) {
+        return index < ANIMALS.size() && findAnimal == false;
+    }
+
+    private static boolean isAnimalInAnimalList(int index, String photo){
+        String regExr = createFinalRegularExpresion(index);
+        boolean foundAnimal = isAnimalFound(photo, regExr);
+
+        return foundAnimal;
+    }
+
+    private static boolean isAnimalFound(String photo, String regExr) {
+        boolean foundAnimal = false;
+
+        Pattern pattern = Pattern.compile(regExr);
+        if(isMatchesPattern(photo, pattern)){
+            foundAnimal = true;
+        }
+        return foundAnimal;
+    }
+
+    private static String createFinalRegularExpresion(int index) {
+        return "^" + createRegExt(index) + "$";
+    }
+
+    private static boolean isMatchesPattern(String photo, Pattern pattern) {
         return pattern.matcher(photo).matches();
     }
 
@@ -67,21 +106,26 @@ public class Main {
         return regExr;
     }
     private static String generateRegExr(String animal){
+        String regExr = firstPartRegExr(animal) + "|" + secondPartRegExr(animal);
+        
+        return regExr;
+    }
+
+    private static String firstPartRegExr(String animal) {
         String regExr = "";
         for(int j = 0; j < animal.length(); j++){
             String charAtX = Character.toString(animal.charAt(j));
             regExr += charAtX + "{1,}";
         }
+        return regExr;
+    }
 
-        regExr += "|";
+    private static String secondPartRegExr(String animal){
+        String regExr = "";
         for(int j = animal.length()-1; j >= 0; j--){
             String charAtX = Character.toString(animal.charAt(j));
             regExr += charAtX + "{1,}";
         }
-
-
-
-
         return regExr;
     }
 }
